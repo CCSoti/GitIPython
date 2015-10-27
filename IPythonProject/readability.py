b4c57ba9993186c8d7906548f7da9fb506491366
Silviya Sotirova <silviya.soti@gmail.com>
2015-10-27 15:05:55 +0000

    Added implementation for catching exception if README.md doesn't exist.

--- IPythonProject/readability.py
+++ IPythonProject/readability.py
@@ -13,11 +13,9 @@ class ReadabilityAnalysis():
 
     def extract_text(self):
         repo_path = os.path.dirname(os.getcwd())  # os.path.dirname returns upper directory from current one
-        print(repo_path)
         os.chdir(repo_path)
         find_file = repo_path + "\\" + self.repo_name
         os.chdir(find_file)
-        print(find_file)
 
         found_path = ""
         for root, dirs, files in os.walk(find_file):
@@ -26,22 +24,21 @@ class ReadabilityAnalysis():
                     found_path = os.path.join(root, file)
 
             for dir in dirs:
-                # print("Dir: ", dir)
                 for root2, dirs2, files2 in os.walk(find_file + "\\" + dir):
                     for file2 in files2:
-                        # print("File2 is:", file2)
                         if file2 == "README.md" and os.path.join(root, file2) is not None:
-                            # print(os.path.join(root, file2))
                             found_path = os.path.join(root2, file2)
-                            # else:
-                            # print("No README.md file.")
 
-        print(found_path)
-        with open(found_path, 'r') as readme_file:
+        readme_text = []
+        try:
+            with open(found_path, 'r') as readme_file:
                 for line in readme_file:
-                    print(line, end='')
+                    readme_text.append(line)
+        except FileNotFoundError:
+            print("Repository doesn't contain README.md file.")
+
+        return readme_text
 
 
-ra = ReadabilityAnalysis("tarmstrong")
+ra = ReadabilityAnalysis("pydata")
 print(ra.extract_text())
-
