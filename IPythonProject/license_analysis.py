ee802c8c00731b7f2864243b035350ab7eb3a7cf
Silviya Sotirova <silviya.soti@gmail.com>
2016-01-13 15:34:53 +0000

    Idea for accessing (second idea) licenses in repositories - extended.

--- IPythonProject/license_analysis.py
+++ IPythonProject/license_analysis.py
@@ -1,9 +1,40 @@
+import os
+
+from git import Repo
+
 from IPythonProject.wrapper import RepositoryWrapper
 
 
-def try_licenses():
-    repo = RepositoryWrapper("vim-ipython", "https://github.com/ivanov/vim-ipython.git")
-    return repo.clone_repos()
+class LicenseAnalysis:
+    def __init__(self, name):
+        self.name = name
+        pass
+
+    def create_repo(self):
+        temp_path = os.path.dirname(os.path.realpath("IPythonProject"))
+        path_project = temp_path + "\GitHubProjects" + "\\" + self.name
+        print("Path: " + path_project)
+
+        repo = Repo.init(path_project)
+        return repo
+
+    def main(self):
+        repo = self.create_repo()
+
+        # explanation for the entries http://gitpython.readthedocs.org/en/stable/reference.html#module-git.index.base
+        repo_dict = repo.index.entries
+        # print(repo.untracked_files)
+        # print(repo.index.entries)
+
+        tree = repo.heads.master.commit.tree
+        print(len(tree))
+        # print(tree.blobs[0].name)
+        for blob in tree.blobs:
+            print(blob.name)
+        for tre in tree:
+            print(tre.name)
 
 
-try_licenses()
+
+repo = LicenseAnalysis("vim-ipython")
+repo.main()
