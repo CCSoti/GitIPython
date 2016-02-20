71e68f8c1815130ddb653a712d89f01469f2569b
Silviya Sotirova <silviya.soti@gmail.com>
2016-02-20 19:24:11 +0000

    Counting number of commits in repositories with GitPython.

--- IPythonProject/wrapper.py
+++ IPythonProject/wrapper.py
@@ -24,4 +24,18 @@ class RepositoryWrapper():
             origin.pull(origin.refs[0].remote_head)
         except:
             print("Could not clone or fetch repository.")
-        # print(repo.index.entries)
+            # print(repo.index.entries)
+
+    def get_num_commits(self):
+        temp_path = os.path.dirname(os.path.realpath("IPythonProject"))
+        path_project = temp_path + "/NewGitHubProjects/" + self.name
+
+        repo = Repo.clone_from(self.url, path_project, branch='master')
+        # origin = repo.create_remote('origin', remote_url)
+
+        num_commits = list(repo.iter_commits('master'))
+        return num_commits
+
+
+rw = RepositoryWrapper("GitIPython", "https://github.com/CCSoti/GitIPython")
+print(len(rw.get_num_commits()))
