0a9bb8870a4ca0516985a1f328ad8f63ffe7cc32
Silviya Sotirova <silviya.soti@gmail.com>
2016-03-21 13:13:07 +0000

    Checks for execution of IPython scripts.

--- IPythonProject/license_analysis.py
+++ IPythonProject/license_analysis.py
@@ -1,11 +1,11 @@
 import codecs
 import json
 import os
-
 import sys
 from git import Repo
 from IPythonProject.wrapper import RepositoryWrapper
 from IPythonProject.readability import ReadabilityAnalysis
+from subprocess import *
 
 
 class LicenseAnalysis:
@@ -44,25 +44,23 @@ class LicenseAnalysis:
             for file in files:
                 if 'π' in file:
                     file = file.replace('π', str('π'.encode("utf-8")))
+
                 if ".ipynb" in file and os.path.join(root, file) is not None:
-                    # print(file)
                     found_path = os.path.join(root, file)
                     ipynb_paths.append(found_path)
-                    # found_path = str(found_path.encode("utf-8"))
-                    print(file)
-                    with open(found_path) as data_file:
+                    with open(found_path, encoding="utf8") as data_file:
                         file_json = json.load(data_file)
                     data_file.close()
                     ipynb_json.append(file_json)
 
-            # for dir in dirs:
-            #     for root2, dirs2, files2 in os.walk(find_file + "\\" + dir):
-            #         for file2 in files2:
-            #             if 'π' in file2:
-            #                 file2 = file2.replace('π', str('π'.encode("utf-8")))
-            #             if ".ipynb" in file2 and os.path.join(root, file2) is not None:
-            #                 found_path = os.path.join(root2, file2)
-            #                 ipynb_paths.append(found_path)
+                    # for dir in dirs:
+                    #     for root2, dirs2, files2 in os.walk(find_file + "\\" + dir):
+                    #         for file2 in files2:
+                    #             if 'π' in file2:
+                    #                 file2 = file2.replace('π', str('π'.encode("utf-8")))
+                    #             if ".ipynb" in file2 and os.path.join(root, file2) is not None:
+                    #                 found_path = os.path.join(root2, file2)
+                    #                 ipynb_paths.append(found_path)
         return ipynb_paths, ipynb_json
 
     def traverse_projects(self):
@@ -87,25 +85,55 @@ class LicenseAnalysis:
         code_no = 0
         all_ratios = []
         for script in ipynb_json:
-            cells = script["worksheets"][0]["cells"]
+            if "worksheets" in script:
+                cells = script["worksheets"][0]["cells"]
+            else:
+                cells = script["cells"]
+
             for cell in cells:
                 cell_type = cell["cell_type"]
                 if cell_type == "code":
                     code_yes += 1
                 else:
                     code_no += 1
-            all_ratios.append(str(code_yes)+":"+str(code_no))
+            all_ratios.append(str(code_yes) + ":" + str(code_no))
             code_yes, code_no = 0, 0
         return all_ratios
+
+    # checking if a IPython script can be executed from the subprocess model
+    def scripts_execution(self, filename):
+        cmd = 'python ' + filename
+        result = Popen(cmd, shell=True, stdout=PIPE)
+        out, err = result.communicate()
+        return result.returncode, out, err
+
+    # checking the outputs -number of errors in a script
+    def outputs(self, ipynb_json):
+        outputs = []
+        for script in ipynb_json:
+            if "worksheets" in script:
+                cells = script["worksheets"][0]["cells"]
+            else:
+                cells = script["cells"]
+
+            for cell in cells:
+                cell_type = cell["outputs"]
+                if cell_type is not []:
+                    for out in cell_type:
+                        outputs.append(out["output_type"])
+
+        return outputs
+
 
 repo = LicenseAnalysis()
 # repos_license_yes, repos_license_no = repo.traverse_projects()
 # print(repos_license_yes)
 # print(repos_license_no)
 repo_path = os.path.dirname(os.getcwd())
-find_file = repo_path + "\IPythonProject\\NewGitHubProjects"
+find_file = repo_path + "\IPythonProject\\NewGitHubProjects2"
 ipynb_paths, ipynb_json = repo.check_ipynb(find_file)
-all_ratios = repo.cell_types(ipynb_json)
-print("Cells ratio(code/no code): ", all_ratios, "\n")
+# all_ratios = repo.cell_types(ipynb_json)
+# print("Cells ratio(code/no code): ", all_ratios, "\n")
 
-# print(ipynb_json[0]["worksheets"][0]["cells"][0]["cell_type"])
+# print(repo.scripts_execution(find_file))
+print(repo.outputs(ipynb_json))
