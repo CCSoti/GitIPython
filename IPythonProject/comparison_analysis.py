a039f16f9e366d7030ff24fb5a9b028f9d1587ff
Silviya Sotirova <silviya.soti@gmail.com>
2016-03-28 15:59:15 +0100

    Comparison analysis - what are the comparisons showing us

---
+++ IPythonProject/comparison_analysis.py
@@ -0,0 +1,14 @@
+import os
+import distance as distance
+from IPythonProject.license_analysis import LicenseAnalysis
+from IPythonProject.comparison import Comparison
+
+
+class ComparisonAnalysis:
+
+    def __init__(self):  # , name):
+        pass
+
+    def scripts_equality(self):
+
+        return
