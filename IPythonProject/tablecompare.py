928b4d690429f7a51461d433c225bb5589565816
Silviya Sotirova <silviya.soti@gmail.com>
2016-04-05 17:56:21 +0100

    Scrip and repo comparison and analysis.

--- IPythonProject/tablecompare.py
+++ IPythonProject/tablecompare.py
@@ -21,6 +21,7 @@ class TableCompare():
         lines = list(lines)
         lines_copy = copy.copy(lines)
         cell_differences = {}
+        repo_cell = ""
         for l in range(0, len(lines)):
             line = lines[l]
             line_diff = []
@@ -39,11 +40,11 @@ class TableCompare():
 
         return cell_differences
 
-    def cell_equality(self):
-        cell_differences = self.get_celldifference()
+    def cell_equality(self, ipython_dict):
+        ipython_dict = self.get_celldifference()
         cell_equality = {}
-        for cell_diff in cell_differences:
-            cell_keys = cell_differences[cell_diff]
+        for cell_diff in ipython_dict:
+            cell_keys = ipython_dict[cell_diff]
             count_equality = 0
             number_lines = len(cell_keys)
             for equality in cell_keys:
@@ -60,14 +61,22 @@ class TableCompare():
         lines = c.execute('SELECT repository, script, line_content FROM ipython ')
         lines = list(lines)
         lines_copy = copy.copy(lines)
-        script_differences = []
+        script_differences = {}
         for l in range(0, len(lines)):
             line = lines[l]
+            line_diff = []
             for c in range(l+1, len(lines_copy)):
                 line_copy = lines_copy[c]
                 if line[0] == line_copy[0] and l != c and line[1] != line_copy[1]:
                     difference = distance.levenshtein(line[2], line_copy[2])
-                    script_differences.append((len(line[2]), len(line_copy[2]), difference))
+                    same = self.cell_analysis(len(line[2]), len(line_copy[2]), difference)
+                    line_diff.append(same)
+
+            repo_cell = str(line[0]) + ": " + str(line[1])
+            if repo_cell in script_differences:
+                script_differences[repo_cell].extend(line_diff)
+            else:
+                script_differences[repo_cell] = line_diff
 
         return script_differences
 
@@ -77,17 +86,28 @@ class TableCompare():
         lines = c.execute('SELECT repository, script, line_content FROM ipython ')
         lines = list(lines)
         lines_copy = copy.copy(lines)
-        repo_differences = []
+        repo_differences = {}
         for l in range(0, len(lines)):
             line = lines[l]
+            line_diff = []
             for c in range(l+1, len(lines_copy)):
                 line_copy = lines_copy[c]
                 if line[0] != line_copy[0] and l != c and line[1] != line_copy[1]:
                     difference = distance.levenshtein(line[2], line_copy[2])
-                    repo_differences.append((len(line[2]), len(line_copy[2]), difference))
+                    same = self.cell_analysis(len(line[2]), len(line_copy[2]), difference)
+                    line_diff.append(same)
+
+            repo_cell = str(line[0]) + ": " + str(line[1])
+            if repo_cell in repo_differences:
+                repo_differences[repo_cell].extend(line_diff)
+            else:
+                repo_differences[repo_cell] = line_diff
 
         return repo_differences
 
 
 tablecompare = TableCompare()
-print(tablecompare.cell_equality())
+# cell_differences = tablecompare.get_celldifference()
+# script_differences = tablecompare.get_scriptdifference()
+repo_differences = tablecompare.get_repodifference()
+print(tablecompare.cell_equality(repo_differences))
