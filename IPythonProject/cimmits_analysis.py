79a53f50b2119ba3ff1c1e1d3fd9e7b1c954a26e
Silviya Sotirova <silviya.soti@gmail.com>
2016-04-15 11:41:33 +0100

    Average and standard deviation for number of commits.

--- IPythonProject/cimmits_analysis.py
+++ IPythonProject/cimmits_analysis.py
@@ -1,4 +1,7 @@
+import csv
 import os
+from statistics import stdev
+
 from git import *
 
 """
@@ -14,25 +17,58 @@ class CommitsAnalysis():
     def get_number_of_commits(self):
         """Method for traversing repositories and counting their number of commits.
         :return:
-            list: [integer, integer,...]
-            A list with the number of commits for each repository."""
+            list: [[repository_name, integer],[...],...]
+            A list with the name of each repository and number of commits for it."""
 
         temp_path = os.path.dirname(os.path.realpath("IPythonProject"))
         path_project = temp_path + "\\NewGitHubProjects\\"
         num_commits = []
 
         for dir in os.listdir(path_project):
-            print("Directory: ", dir)
             for d in os.listdir(path_project + "\\" + dir):
-                print("Directory inside: ", d)
                 repo = Repo(path_project + "\\" + dir + "\\" + d, search_parent_directories=True)
-                print(repo.active_branch.is_valid())
                 if repo.active_branch.is_valid():
                     commits = list(repo.iter_commits())
-                    num_commits.append(len(commits))
+                    num_commits.append([dir, len(commits)])
 
         return num_commits
 
+    def store_number_of_commits_in_file(self):
+        """Method for storing the name of the repository and its number of commits
+        in a CSV file.
+        :return:
+            CSV file: number_of_commits.csv"""
+
+        num_commmits = self.get_number_of_commits()
+        with open("number_of_commits.csv", "w") as commits_file:
+            spamwriter = csv.writer(commits_file)
+            for commits in num_commmits:
+                spamwriter.writerow(commits)
+        commits_file.close()
+
+    def calculate_average_of_commits(self):
+        """Method for calculating the average of all commits.
+        :return:
+            Number: Integer"""
+
+        number_of_commits = self.get_number_of_commits()
+        sum = 0
+        for commits in number_of_commits:
+            sum += commits[1]
+        average_commit = sum/len(number_of_commits)
+
+    def calculate_standard_deviation(self):
+        """Method for calculating the standard deviation of all commits.
+        :return:
+            Number: Integer"""
+        commits_values = []
+        number_of_commits = self.get_number_of_commits()
+        for commit in number_of_commits:
+            commits_values.append(commit[1])
+
+        standard_deviation = stdev(commits_values)
+        return standard_deviation
+
 
 commits_analysis = CommitsAnalysis()
-print(commits_analysis.get_number_of_commits())
+print(commits_analysis.calculate_standard_deviation())
